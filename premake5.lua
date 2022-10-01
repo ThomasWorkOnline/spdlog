9cb55796c44c09bc8dfb33f4d6263133e59dc325
Thomas Lessard <thomaslessard122@hotmail.com>
2022-10-01 17:16:52 -0400

    Remove optimize "Off" and moved to master branch

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "Off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	defines
+	{
+		"SPDLOG_COMPILED_LIB"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
+
+	filter { ("configurations:Release" or "configurations:Dist") and "system:windows" }
+		flags
+		{
+			"LinkTimeOptimization"
+		}
